0cdac2c3da85715ce7b6a11ecd882d12ed900a4c
LudovicTonneaux <LudovicTonneaux@gmail.com>
2017-04-29 17:13:04 +0200

    CRUD API
    Werkende CRUD API met codeigniter en bootstrap.
    Zie ProjectWebAdvanced.sql voor de database

---
+++ CRUD/application/views/person/add.php
@@ -0,0 +1,23 @@
+<?php echo validation_errors(); ?>
+<?php echo form_open('person/add',array("class"=>"form-horizontal")); ?>
+
+	<div class="form-group">
+		<label for="first_name" class="col-md-4 control-label">First Name</label>
+		<div class="col-md-8">
+			<input type="text" name="first_name" value="<?php echo $this->input->post('first_name'); ?>" class="form-control" id="first_name" />
+		</div>
+	</div>
+	<div class="form-group">
+		<label for="last_name" class="col-md-4 control-label">Last Name</label>
+		<div class="col-md-8">
+			<input type="text" name="last_name" value="<?php echo $this->input->post('last_name'); ?>" class="form-control" id="last_name" />
+		</div>
+	</div>
+	
+	<div class="form-group">
+		<div class="col-sm-offset-4 col-sm-8">
+			<button type="submit" class="btn btn-success">Save</button>
+        </div>
+	</div>
+
+<?php echo form_close(); ?>
