e62ed43d1c767e9c3911f6504e30757514cdb277
pavonedavide <dj_pavone@hotmail.com>
2017-05-03 16:46:35 +0200

    alle zever bold van CRUD

--- CRUD/application/views/home_view.php
+++ CRUD/application/views/home_view.php
@@ -1,13 +1,21 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-    <title>Simple Login with CodeIgniter - Private Area</title>
+    <title>Login - Monkey Business</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSS styles -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
+
+    <!-- JS Libs -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.js" type="text/javascript"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" type="text/javascript"></script>
 </head>
 <body>
 <h1>Home</h1>
 <h2>Welcome <?php echo $username; ?>!</h2>
 <h3><?php echo anchor('person', 'Person', 'class="link-class"') ?></h3>
 <h3><?php echo anchor('event', 'Event', 'class="link-class"') ?></h3>
-<a href="home/logout">Logout</a>
+<a href="home/logout" class="btn btn-success">Logout</a>
 </body>
 </html>
--- CRUD/application/views/login_view.php
+++ CRUD/application/views/login_view.php
@@ -1,19 +1,27 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-    <title>Simple Login with CodeIgniter</title>
+    <title>login - monkey business</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSS styles -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
+
+    <!-- JS Libs -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.js" type="text/javascript"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" type="text/javascript"></script>
 </head>
 <body>
-<h1>Simple Login with CodeIgniter</h1>
+<h1>login - monkey business</h1>
 <?php echo validation_errors(); ?>
 <?php echo form_open('verifylogin'); ?>
 <label for="username">Username:</label>
 <input type="text" size="20" id="username" name="username"/>
 <br/>
 <label for="password">Password:</label>
-<input type="password" size="20" id="passowrd" name="password"/>
+<input type="password" size="20" id="password" name="password"/>
 <br/>
-<input type="submit" value="Login"/>
+<input type="submit" value="Login" class="btn btn-success"/>
 </form>
 </body>
 </html>
