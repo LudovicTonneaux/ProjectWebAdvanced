227f60b6bf62b0bde3f4f5963d0c47d7f5148fb4
pavonedavide <dj_pavone@hotmail.com>
2017-05-12 11:56:57 +0200

    verder afmaken van de api op datum
    ik weet niet waarom het niet werkt, het zou moeten lukken???
    
    kan iemand van jullie even checken?

--- ApiLudo.php
+++ ApiLudo.php
@@ -6,50 +6,56 @@
  * Time: 15:58
  */
 
-$user='pxluser';
-$password='KKRuA3YFxIeQw!';
-$database='projectWeb';
+$user = 'pxluser';
+$password = 'KKRuA3YFxIeQw!';
+$database = 'projectWeb';
 $hostname = 'localhost';
 $pdo = null;
 
 try {
-    $pdo = new PDO( "mysql:host=$hostname;dbname=$database",
-        $user, $password );
-    $pdo->setAttribute( PDO::ATTR_ERRMODE,
-        PDO::ERRMODE_EXCEPTION );
-
+    $pdo = new PDO("mysql:host=$hostname;dbname=$database",
+        $user, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE,
+        PDO::ERRMODE_EXCEPTION);
 
 
     switch ($_SERVER['REQUEST_METHOD']) {
         case 'GET':
-            if (isset($_GET['table']) AND !isset($_GET['id']))
-            {
+            if (isset($_GET['table']) AND !isset($_GET['id']) AND isset($_GET['from']) AND isset($_GET['until'])) {
+                //query die perfect werkt op de server zelf:
+                //SELECT * FROM events WHERE datum BETWEEN '2017-03-03' AND '2017-05-23' ORDER BY datum ASC;
+                $statement = $pdo->prepare('SELECT * FROM ' . $_GET['table'] . ' WHERE datum BETWEEN ' . $_GET['from'] . ' AND ' . $_GET['until'] . ' ORDER BY datum ASC');
+                $statement->execute();
+                $results = $statement->fetchAll(PDO::FETCH_ASSOC);
+                $json = json_encode($results);
+
+                //checken van de vars
+                echo $_GET['table'] . "<br />";
+                echo $_GET['from'] . "<br />";
+                echo $_GET['until'];
+            } elseif (isset($_GET['table']) AND !isset($_GET['id'])) {
                 $statement = $pdo->prepare('SELECT * from ' . $_GET['table']);
                 $statement->execute();
-                $results=$statement->fetchAll(PDO::FETCH_ASSOC);
-                $json=json_encode($results);
+                $results = $statement->fetchAll(PDO::FETCH_ASSOC);
+                $json = json_encode($results);
                 echo $json;
-            }
-
-            elseif (isset($_GET['table']) AND isset($_GET['id']))
-            {
+            } elseif (isset($_GET['table']) AND isset($_GET['id'])) {
                 $statement = $pdo->prepare('SELECT * from ' . $_GET['table'] . ' WHERE id = ' . $_GET['id']);
                 $statement->execute();
-                $results=$statement->fetchAll(PDO::FETCH_ASSOC);
-                $json=json_encode($results);
+                $results = $statement->fetchAll(PDO::FETCH_ASSOC);
+                $json = json_encode($results);
                 echo $json;
             }
             break;
         case 'PUT':
-            if (isset($_GET['table']) AND isset($_GET['id']) AND isset($_GET['first_name']) AND isset($_GET['last_name']))
-            {
-                $statement = $pdo->prepare('UPDATE ' . $_GET['table'] . ' SET first_name = "' . $_GET['first_name'] . '", last_name = "' . $_GET['last_name'] . '" WHERE id = '. $_GET['id']);
+            if (isset($_GET['table']) AND isset($_GET['id']) AND isset($_GET['first_name']) AND isset($_GET['last_name'])) {
+                $statement = $pdo->prepare('UPDATE ' . $_GET['table'] . ' SET first_name = "' . $_GET['first_name'] . '", last_name = "' . $_GET['last_name'] . '" WHERE id = ' . $_GET['id']);
                 $statement->debugDumpParams();
                 $statement->execute();
                 $statement = $pdo->prepare('SELECT * from ' . $_GET['table'] . ' WHERE id = ' . $_GET['id']);
                 $statement->execute();
-                $results=$statement->fetchAll(PDO::FETCH_ASSOC);
-                $json=json_encode($results);
+                $results = $statement->fetchAll(PDO::FETCH_ASSOC);
+                $json = json_encode($results);
                 echo $json;
             }
             break;
@@ -60,10 +66,8 @@ try {
 
             break;
     }
-}
-catch ( PDOException $e ) {
+} catch (PDOException $e) {
     print 'Exception!: ' . $e->getMessage();
-}
-finally {
+} finally {
     $pdo = null;
 }
