a25613b1096d418dd96b0fc0dee76cac89570d34
LudovicTonneaux <LudovicTonneaux@gmail.com>
2017-05-19 16:51:13 +0200

    wp3

--- fetch_wp3.js
+++ fetch_wp3.js
@@ -1,7 +1,19 @@
 /**
  * Created by Davide Pavone on 19/05/2017.
  */
-var request = new Request('http://192.168.118.148/~user/Project/ApiLudo.php?table=events', {
+
+var request = new Request('http://172.16.140.128/~user/ProjectWebAdvanced/ApiLudo.php?table=event&name=wp3&date=2045-02-03&person_id=1', {
+    method: 'POST'
+});
+
+fetch(request).catch(function (err) {
+    console.log(err.message);
+});
+
+//hier moet er een second of 2 pauze zijn zodat die post altijd wordt weergegeven
+
+
+var request = new Request('http://172.16.140.128/~user/ProjectWebAdvanced/ApiLudo.php?table=event', {
     method: 'GET'
 });
 
@@ -9,19 +21,12 @@ fetch(request).then(function (response) {
     return response.json();
 }).then(function (response2) {
     console.log('JSON: ', response2);
-    for (var i = 0; i < response2.length; i++) {
-        console.log(response2[i]);
+        for (var i = 0; i < response2.length; i++) {
+            console.log(response2[i]);
+            document.write(JSON.stringify(response2[i]));
     }
-    this.getJSON(response2.json(), function (data) {
-        this.each(data, function (i, f) {
-            var tblRow = "<tr>" + "<td>" + f.eventID + "</td>" +
-                "<td>" + f.persoonID + "</td>" + "<td>" + f.datum + "</td>" + "</tr>";
-            this(tblRow).appendTo("#userdata tbody");
-        });
 
-    });
 
 }).catch(function (err) {
     console.log(err.message);
 });
-
