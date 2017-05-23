db4131cec71f36db60ae5c617a6ce2e82ad70d9a
fredericcoucke1996 <frederic.coucke@student.pxl.be>
2017-05-23 14:32:28 +0200

    unit tests_af

--- test/controller/EventsControllerTest.php
+++ test/controller/EventsControllerTest.php
@@ -11,8 +11,6 @@ class EventsControllerTest extends PHPUnit\Framework\Testcase
 
     public function setUP()
     {
-        $pdo = null;
-
         try {
         $xml = simplexml_load_file("configDatabank_wp6.xml");// or die("Error: Cannot create xml object");
 
@@ -99,6 +97,172 @@ class EventsControllerTest extends PHPUnit\Framework\Testcase
 
         $this->assertNotNull($results);
     }
+
+    public function TestPutEventname()
+    {
+        $name = "test";
+        $id = 1;
+
+        $statement = $this->pdo->prepare('UPDATE event SET name = ' . $name . '  WHERE id = ' . $id);
+        $statement->execute();
+
+        $statement2 = $this->pdo->prepare('SELECT name FROM event WHERE id = ' . $id);
+        $statement2->execute();
+        $result = $statement2->fetchAll(PDO::FETCH_ASSOC);
+
+        $this->assertEqual($name,$result);
+
+    }
+
+    public function TestPutEventdate()
+    {
+        $date = "2000-01-01";
+        $id = 1;
+
+        $statement = $this->pdo->prepare('UPDATE event SET date = ' . $date . '  WHERE id = ' . $id);
+        $statement->execute();
+
+        $statement2 = $this->pdo->prepare('SELECT date FROM event WHERE id = ' . $id);
+        $statement2->execute();
+        $result = $statement2->fetchAll(PDO::FETCH_ASSOC);
+
+        $this->assertEqual($date,$result);
+
+    }
+
+    public function TestPutEventPerson_id()
+    {
+        $person_id = 10;
+        $id = 1;
+
+        $statement = $this->pdo->prepare('UPDATE event SET person_id = ' . $person_id . '  WHERE id = ' . $id);
+        $statement->execute();
+
+        $statement2 = $this->pdo->prepare('SELECT person_id FROM event WHERE id = ' . $id);
+        $statement2->execute();
+        $result = $statement2->fetchAll(PDO::FETCH_ASSOC);
+
+        $this->assertEqual($person_id,$result);
+
+    }
+
+    public function TestPutPersonFirstName()
+    {
+        $firstname = "test";
+        $id = 1;
+
+        $statement = $this->pdo->prepare('UPDATE person SET first_name = ' . $firstname . '  WHERE id = ' . $id);
+        $statement->execute();
+
+        $statement2 = $this->pdo->prepare('SELECT first_name FROM person  WHERE id = ' . $id);
+        $statement2->execute();
+        $result = $statement2->fetchAll(PDO::FETCH_ASSOC);
+
+        $this->assertEqual($firstname,$result);
+
+    }
+
+    public function TestPutPersonLastName()
+    {
+        $lastname = "test";
+        $id = 1;
+
+        $statement = $this->pdo->prepare('UPDATE person SET last_name = ' . $lastname . '  WHERE id = ' . $id);
+        $statement->execute();
+
+        $statement2 = $this->pdo->prepare('SELECT last_name FROM person WHERE id = ' . $id);
+        $statement2->execute();
+        $result = $statement2->fetchAll(PDO::FETCH_ASSOC);
+
+        $this->assertEqual($lastname,$result);
+
+    }
+
+    public function TestPostEvent()
+    {
+
+        $statement2 = $this->pdo->prepare('SELECT name FROM event');
+        $statement2->execute();
+        $result = $statement2->fetchAll(PDO::FETCH_ASSOC);
+
+        $size1 = $result.size;
+
+        $date = "2000-01-01";
+        $name = "test";
+        $person_id = 10;
+
+        $statement = $this->pdo->prepare('INSERT INTO ' . $_GET['table'] . ' (id, name, date, person_id) VALUES (NULL, ' . $name . ',' . $date . ',' . $person_id . ') ');
+        $statement->execute();
+        $statement->debugDumpParams();
+
+        $statement2 = $this->pdo->prepare('SELECT name FROM event');
+        $statement2->execute();
+        $result = $statement2->fetchAll(PDO::FETCH_ASSOC);
+
+        $size2 = $result.size;
+
+        $this->assertEqual($size1,$size2-1);
+
+    }
+
+    public function TestPostPerson()
+    {
+        $statement2 = $this->pdo->prepare('SELECT first_name FROM person');
+        $statement2->execute();
+        $result = $statement2->fetchAll(PDO::FETCH_ASSOC);
+
+        $size1 = $result.size;
+
+        $first_name = "test";
+        $last_name = "test";
+
+        $statement = $this->pdo->prepare('INSERT INTO ' . $_GET['table'] . ' (id, first_name, last_name) VALUES (NULL, ' . $first_name . ',' . $last_name . ') ');
+        $statement->execute();
+        $statement->debugDumpParams();
+
+        $statement2 = $this->pdo->prepare('SELECT name FROM person');
+        $statement2->execute();
+        $result = $statement2->fetchAll(PDO::FETCH_ASSOC);
+
+        $size2 = $result.size;
+
+        $this->assertEqual($size1,$size2-1);
+
+    }
+
+    public function TestDeleteEvent()
+    {
+        $id = 1;
+
+        $statement = $this->pdo->prepare('DELETE from event WHERE id = ' . $id);
+        $statement->execute();
+
+        $statement2 = $this->pdo->prepare('SELECT * FROM event WHERe id = ' . $id);
+        $statement2->execute();
+        $result = $statement2->fetchAll(PDO::FETCH_ASSOC);
+
+        $this->assertNull($result);
+
+    }
+
+    public function TestDeletePerson()
+    {
+        $id = 1;
+
+        $statement = $this->pdo->prepare('DELETE from person WHERE id = ' . $id);
+        $statement->execute();
+
+        $statement2 = $this->pdo->prepare('SELECT * FROM person WHERe id = ' . $id);
+        $statement2->execute();
+        $result = $statement2->fetchAll(PDO::FETCH_ASSOC);
+
+        $this->assertNull($result);
+    }
+
+
+
+
+
 
 
 }
